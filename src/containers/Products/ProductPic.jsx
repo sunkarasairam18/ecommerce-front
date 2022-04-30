8c617a6d475fa624284fb7059d2e031ef4b76c67
sunkarasairam18 <sunkarasairam@icloud.com>
2022-04-30 22:37:24 +0530

    30 April 2022

--- src/containers/Products/ProductPic.jsx
+++ src/containers/Products/ProductPic.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProductPic = ({url}) => {
     return ( 
         <div style={{height:"100px",width:"100px",margin:"5px",borderRadius:"4px",display:"grid",placeItems:"center",overflow:"hidden"}}>
-            <img src={`http://localhost:3000/public/${url}` } alt="pic" style={{height:"90%",width:"100%",borderRadius:"4px"}}/>
+            <img src={`https://mern-ecommerce-products-images.s3.amazonaws.com/${url}` } alt="pic" style={{maxHeight:"100%",maxWidth:"100%",borderRadius:"4px",objectFit:"contain"}}/>
             
         </div>
     );
